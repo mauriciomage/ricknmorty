b7533b4a5b02ce78561bf8e59a74a626445b2e5d
Mauricio <mauriciomage@gmail.com>
2024-02-27 12:33:37 +0100

    MM, unit tests for status component

--- src/app/shared/status/status.component.spec.ts
+++ src/app/shared/status/status.component.spec.ts
@@ -1,23 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { StatusComponent } from './status.component';
+import { Status } from '../../app.constant';
 
 describe('StatusComponent', () => {
   let component: StatusComponent;
   let fixture: ComponentFixture<StatusComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [StatusComponent]
-    })
-    .compileComponents();
-    
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [StatusComponent],
+    });
     fixture = TestBed.createComponent(StatusComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set isAlive to true when status is "alive"', () => {
+    component.status = Status.ALIVE;
+    component.ngOnInit();
+    expect(component.isAlive).toBe(true);
+    expect(component.isUnknown).toBe(false);
+  });
+
+  it('should set isUnknown to true when status is "unknown"', () => {
+    component.status = Status.UNKNOWN;
+    component.ngOnInit();
+    expect(component.isAlive).toBe(false);
+    expect(component.isUnknown).toBe(true);
+  });
+
+  it('should convert status to lowercase during initialization', () => {
+    component.status = 'ALiVe'; // Mixed case
+    component.ngOnInit();
+    expect(component.status).toBe(Status.ALIVE.toLowerCase());
+  });
+
+  it('should handle not match any status', () => {
+    component.status = 'invalid_status';
+    component.ngOnInit();
+    expect(component.isAlive).toBe(false);
+    expect(component.isUnknown).toBe(false);
   });
 });
